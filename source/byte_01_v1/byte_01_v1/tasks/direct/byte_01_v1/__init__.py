c3a0c0225ecf830c71b2f0177e1b856c42cdf8ee
Your Name <your_email@example.com>
2026-04-01 11:43:01 +0530

    feat: initial BYTE-01 V1 RL training environment setup

---
+++ source/byte_01_v1/byte_01_v1/tasks/direct/byte_01_v1/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Byte-01-V1-Direct-v0",
+    entry_point=f"{__name__}.byte_01_v1_env:Byte01V1Env",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.byte_01_v1_env_cfg:Byte01V1EnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+        "skrl_amp_cfg_entry_point": f"{agents.__name__}:skrl_amp_cfg.yaml",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
